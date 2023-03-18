11aa949a7fc0595e1906127d12d76fcdb4269aeb
rusibana <desurerusibana@gmail.com>
2023-03-18 01:03:58 -0700

    this is me

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - starting
+ * description: prints using printf
+ * Return: return 0 when success
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcomeis a piece of art,");
+	return (0); }
