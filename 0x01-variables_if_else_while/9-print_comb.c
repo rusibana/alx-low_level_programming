ceb156c10920f661568e49c1fe6c867264564ca3
rusibana <desurerusibana@gmail.com>
2023-03-19 13:12:05 -0700

    day of rest
    dgfg

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * separated by ", "
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+	putchar(i + '0');
+
+if (i != 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+
+putchar('\n');
+return (0);
+}
+
