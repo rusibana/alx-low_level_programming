7e426c60582ccc4815a8bb37d6754ce6d8efedcf
rusibana <desurerusibana@gmail.com>
2023-03-19 12:19:53 -0700

    df f

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints single digit numbers of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int num;
+
+    for (num = 0; num < 10; num++)
+    {
+        putchar(num + '0');
+    }
+    putchar('\n');
+
+    return (0);
+}
+
