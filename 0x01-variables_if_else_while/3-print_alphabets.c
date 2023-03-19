400dae9b9f83b1a550b5ff865f8fba52fcf2dc06
rusibana <desurerusibana@gmail.com>
2023-03-19 10:16:18 -0700

    the message

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ *
+ * Return: Always 0 for successful
+ *
+ * */
+int main(void){
+char lowercase = 'a';
+char uppercase = 'Z';
+
+while(lowercase <= 'z')
+{
+putchar(lowercase);
+lowercase++;
+}
+
+while(uppercase <= 'Z')
+{
+putchar(uppercase);
+uppercase++;
+}
+return (0);
+}
